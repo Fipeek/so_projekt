4790745a4854b23330a35810fcadaec2d3bd7ac0
Szymon Fiedosiuk <84189269+Amooxiz@users.noreply.github.com>
2022-04-11 12:16:06 +0200

    Iterowanie po plikach w katalogach

--- Projekt.c
+++ Projekt.c
@@ -1,21 +1,64 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
 #include <dirent.h>
-#include <stdbool.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-bool isDir(DIR *dir);
-
-
-void main(int argc, char *argv[])
+void compareFiles(char* sourceDirName, char* destinationDirName)
 {
-	DIR *sourceDir = opendir(argv[1]);
-	DIR *destinationDir = opendir(argv[2]);
-	if(!isDir(sourceDir)){
-	printf("plik źródłowy nie jest katalogiem\n");
+	DIR* sD, dD;
+	struct dirent* sF, dF;
+	sD = opendir(sourceDirName);
+	dD = opendir(destinationDirName);
+	char* sourcePath[100];
+	char* destinationPath[100];
+	bool IsFound = false;
+	while ((sF = readdir(sD)) != NULL)
+	{
+		struct stat sFStats;
+		IsFound = false;
+		sprintf(sourcePath, "%s/%s", sourceDirName, sF->d_name);
+		if (stat(sourcePath, &sFStats) == -1)
+		{
+			printf("Problem z plikiem o nazwie: %s\n", sourcePath);
+			continue;
+		}
+
+		if ((sFStats.st_mode & S_IFMT) == S_IFDIR)
+		{
+			continue;
+		}
+		else
+		{
+			while ((dF = readdir(dD)) != NULL)
+			{
+				struct stat dFStats;
+				sprintf(destinationPath, "%s/%s", destinationDirName, dF->d_name);
+				if (stat(destinationPath, &dFStats) == -1)
+				{
+					printf("Problem z plikiem o nazwie: %s\n", destinationPath);
+					continue;
+				}
+
+				if ((dFStats.st_mode & S_IFMT) == S_IFDIR) // pomijanie katalog�w
+				{
+					continue;
+				}
+				else
+				{
+					if (strcmp(sF->d_name, dF->d_name) == 0)
+					{
+						if (difftime(sFStats.st_mtime, dFStats.st_mtime) != 0) // jezeli daty modyfikacji rozne
+						{
+							//kopiowanie ze zrodlowego do docelowego
+							IsFound = true;
+						}
+					}
+				}
+			}
+			if (!IsFound) // jezeli nie ma w docelowym
+			{
+				//kopiowanie ze zrodlowego do docelowego
+			}
+		}
 	}
-	if(!isDir(destinationDir)){
-	printf("plik docelowy nie jest katalogiem\n");
-	}
-	
 }

8798eedfe2934441c546c08c9235937565d37eb2
fetus-elitus <81877956+fetus-elitus@users.noreply.github.com>
2022-04-25 16:18:47 +0200

    Add files via upload

--- CopyDelete.c
+++ CopyDelete.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <stdbool.h>
+#include <utime.h>
 
 
 bool copyFile(char *sourcePath, char *targetPath)
@@ -9,6 +10,8 @@ bool copyFile(char *sourcePath, char *targetPath)
 	int buffSize = sizeof(char);
 	int packetsRead;
 	char buffer[buffSize]; 
+	printf("%s\n", sourcePath);
+	printf("%s\n", targetPath);
 	
    	FILE *sourceFile = fopen(sourcePath, "r");
    	FILE *targetFile = fopen(targetPath, "w");
@@ -17,7 +20,8 @@ bool copyFile(char *sourcePath, char *targetPath)
   		return false;
 	if(!targetFile)
 		return false;
-  
+  	printf("\n Oba pliki poprawne");
+  	
 	while(packetsRead = fread(buffer, buffSize, numberOfPackets, sourceFile))
 	{
 		fwrite(buffer, sizeof(char), packetsRead, targetFile);
@@ -29,10 +33,14 @@ bool copyFile(char *sourcePath, char *targetPath)
 			return false;
 		}
 	}
-   	 
+   	printf("\nPrzed zamknieciem pliku \n");
+   	
  	fclose(targetFile);
 	fclose(sourceFile);
+	return true;
 }
+
+
 
 bool deleteFile(char *targetFile)
 {
